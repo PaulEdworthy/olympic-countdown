f109c20f6f41f9d4fb646f5e0e77e461df811273
paul <pauledworthy@gmail.com>
2022-09-10 16:23:48 -0400

    late commit

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const targetDate = new Date('July 23, 2021 07:00:00').getTime();
+const targetDate = new Date('February 4, 2022 07:00:00').getTime();
 const timer = document.getElementById('timer');
 const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 
-let interval = setInterval(function () {
-  let now = new Date().getTime();
+const interval = setInterval(function () {
+  const now = new Date().getTime();
 
-  let timeRemaining = targetDate - now;
+  const timeRemaining = targetDate - now;
 
-  let daysRemaining = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-  let hoursRemaining = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutesRemaining = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-  let secondsRemaining = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  const daysRemaining = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hoursRemaining = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutesRemaining = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const secondsRemaining = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
   days.textContent = daysRemaining;
   hours.textContent = hoursRemaining;
